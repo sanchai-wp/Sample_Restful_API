e3340b7e9d4aee780dbdf479bb05c430992eb6b4
Sanchai Wora-apinyaporn <sanchai_wp@hotmail.com>
2018-08-13 11:16:34 +0700

    add validate process

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,6 +10,16 @@ use Validator;
 use DB;
 class TaskController extends Controller
 {
+    public function taskValidate($request)
+    {
+        $validator = Validator::make($request->all(), [
+            'subject' => 'required|max:50',
+            'content' => 'nullable|max:255',
+            'status' => 'in:pending,done',
+        ]);
+        return $validator;
+    }
+
     public function index()
     {
         //Get all task
@@ -18,12 +28,24 @@ class TaskController extends Controller
     
     public function store(Request $request)
     {
+        $validator = $this->taskValidate($request);
+        if ($validator->fails()) {
+            //return json_encode($validator->errors()->all());
+            return response()->json(['errors' => $validator->errors()->all(), 'status' => Response::HTTP_BAD_REQUEST]);
+        }
+
         $id = TaskModel::create($request->all());
         return response()->json(['id' => $id, 'status' => Response::HTTP_CREATED]);
     }
  
     public function update(Request $request, TaskModel $id)
     {
+        $validator = $this->taskValidate($request);
+        if ($validator->fails()) {
+            //return json_encode($validator->errors()->all());
+            return response()->json(['errors' => $validator->errors()->all(), 'status' => Response::HTTP_BAD_REQUEST]);
+        }
+        
         $id->update($request->all());
         return response()->json(['id' => $id, 'status' => Response::HTTP_OK]);
     }
